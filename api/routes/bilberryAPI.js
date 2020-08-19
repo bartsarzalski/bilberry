ca2b06714c11ca678cb3ceb4c8e149375f9a9818
Bartek <bartek@MacBook-Pro-Bartek.local>
2020-08-19 08:32:14 +0200

    initial commit

---
+++ api/routes/bilberryAPI.js
@@ -0,0 +1,10 @@
+const bilberryAPI = require('../demo');
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    res.send("API works.");
+});
+
+module.exports = router;
